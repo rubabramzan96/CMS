859d0b5ea4ae0ec8ffe957aa0de47b37377d3a17
rubabramzan96 <rubabramzan96@gmail.com>
2017-12-22 19:01:29 +0000

    Event & EventCat controller added

---
+++ CMS/Controllers/EventCategoriesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CMS.Models.CMSModel;
+
+namespace CMS.Controllers
+{
+    public class EventCategoriesController : Controller
+    {
+        private CMSContext db = new CMSContext();
+
+        // GET: EventCategories
+        public ActionResult Index()
+        {
+            return View(db.EventCategories.ToList());
+        }
+
+        // GET: EventCategories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EventCategory eventCategory = db.EventCategories.Find(id);
+            if (eventCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eventCategory);
+        }
+
+        // GET: EventCategories/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EventCategories/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EventCatID,Name,Outdoor,Family")] EventCategory eventCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.EventCategories.Add(eventCategory);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(eventCategory);
+        }
+
+        // GET: EventCategories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EventCategory eventCategory = db.EventCategories.Find(id);
+            if (eventCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eventCategory);
+        }
+
+        // POST: EventCategories/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EventCatID,Name,Outdoor,Family")] EventCategory eventCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(eventCategory).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(eventCategory);
+        }
+
+        // GET: EventCategories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EventCategory eventCategory = db.EventCategories.Find(id);
+            if (eventCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eventCategory);
+        }
+
+        // POST: EventCategories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            EventCategory eventCategory = db.EventCategories.Find(id);
+            db.EventCategories.Remove(eventCategory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CMS/Controllers/EventsController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CMS.Models.CMSModel;
+
+
+namespace CMS.Controllers
+{
+    public class EventsController : Controller
+    {
+        private CMSContext db = new CMSContext();
+
+        // GET: Events
+        public ActionResult Index()
+        {
+             var events = db.Events.Include(EC => EC.associatedEvent).Include(EC => EC.associatedLocation);
+             return View(events.ToList());
+           
+        }
+
+        // GET: Events/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Event @event = db.Events.Find(id);
+            if (@event == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@event);
+        }
+
+        // GET: Events/Create
+        public ActionResult Create()
+        {
+            ViewBag.EventCatID = new SelectList(db.EventCategories, "EventCatID", "Name");
+            ViewBag.LocationID = new SelectList(db.Locations, "LocationID", "Name");
+            return View();
+        }
+
+        // POST: Events/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EventID,EventCatID,LocationID,Name,StartTime,EndTime,OrganiserID")] Event @event)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Events.Add(@event);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.EventCatID = new SelectList(db.EventCategories, "EventCatID", "Name", @event.EventCatID);
+            ViewBag.LocationID = new SelectList(db.Locations, "LocationID", "Name", @event.LocationID);
+            return View(@event);
+        }
+
+        // GET: Events/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Event @event = db.Events.Find(id);
+            if (@event == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.EventCatID = new SelectList(db.EventCategories, "EventCatID", "Name", @event.EventCatID);
+            ViewBag.LocationID = new SelectList(db.Locations, "LocationID", "Name", @event.LocationID);
+            return View(@event);
+        }
+
+        // POST: Events/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EventID,EventCatID,LocationID,Name,StartTime,EndTime,OrganiserID")] Event @event)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(@event).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.EventCatID = new SelectList(db.EventCategories, "EventCatID", "Name", @event.EventCatID);
+            ViewBag.LocationID = new SelectList(db.Locations, "LocationID", "Name", @event.LocationID);
+            return View(@event);
+        }
+
+        // GET: Events/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Event @event = db.Events.Find(id);
+            if (@event == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@event);
+        }
+
+        // POST: Events/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Event @event = db.Events.Find(id);
+            db.Events.Remove(@event);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
