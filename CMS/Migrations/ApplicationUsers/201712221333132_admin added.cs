6b643ed88a646f95f313cc3e551a95666e01195a
rubabramzan96 <rubabramzan96@gmail.com>
2017-12-22 13:36:09 +0000

    admin added :)

---
+++ CMS/Migrations/ApplicationUsers/201712221333132_admin added.cs
@@ -0,0 +1,16 @@
+namespace CMS.Migrations.ApplicationUsers
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class adminadded : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- CMS/Migrations/ApplicationUsers/Configuration.cs
+++ CMS/Migrations/ApplicationUsers/Configuration.cs
@@ -1,5 +1,8 @@
 namespace CMS.Migrations.ApplicationUsers
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -15,18 +18,33 @@ namespace CMS.Migrations.ApplicationUsers
 
         protected override void Seed(CMS.Models.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            var manager =
+        new UserManager<ApplicationUser>(
+            new UserStore<ApplicationUser>(context));
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            var roleManager =
+                new RoleManager<IdentityRole>(
+                    new RoleStore<IdentityRole>(context));
+
+            roleManager.Create(new IdentityRole { Name = "Admin" });
+            context.Users.AddOrUpdate(u => u.Email, new ApplicationUser
+            {
+                Email = "S12345678@mail.itsligo.ie",
+                EmailConfirmed = true,
+                UserName = "S12345678@mail.itsligo.ie",
+                PasswordHash = new PasswordHasher().HashPassword("Ss1234567$1"),
+                SecurityStamp = Guid.NewGuid().ToString(),
+            });
+            context.SaveChanges();
+            ApplicationUser admin = manager.FindByEmail("S12345678@mail.itsligo.ie");
+            if (admin != null)
+            {
+                manager.AddToRoles(admin.Id, new string[] { "Admin" });
+            }
+            else
+            {
+                throw new Exception { Source = "Did not find user" };
+            }
         }
     }
 }
